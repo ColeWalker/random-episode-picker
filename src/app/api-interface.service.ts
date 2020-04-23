a56e99bf9789f8916545dbb324cc713a750824f2
ColeWalker <colegrwalker@gmail.com>
2020-04-23 12:23:03 -0400

    remove unnecessary import

--- src/app/api-interface.service.ts
+++ src/app/api-interface.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { movieDbToken } from '../../credentials';
+
 
 @Injectable({
   providedIn: 'root'
